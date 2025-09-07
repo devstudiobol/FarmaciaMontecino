9353a542168e8455e96175f556d9b0c12fc40706
Jose Miguel <miki.por.ll99@gmail.com>
2025-09-07 19:33:47 -0400

    Update Producto.cs

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -33,6 +33,7 @@ namespace Farmacia.Models
         public int idtipo { get;set; }
         [JsonIgnore]
         public Tipo Tipo { get; set; }
+        	public int Precio_compra { get; set; }
 
     }
 }
